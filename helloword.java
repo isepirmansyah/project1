ec5bb5e1acffedd5bac85f55cf17551ca53f22f8
isepirmansyah <77084994+isepirmansyah@users.noreply.github.com>
2021-01-19 09:28:08 +0700

    Update helloword.java
    Penambahan data baru coyy

--- helloword.java
+++ helloword.java
@@ -1,6 +1,10 @@
 public class HelloWord {
   public static void main(){
     System.out.println("Hello Word");
+    int angka1 = 20;
+    int angka2 = 30;
+    int jumlah = angka1 * angka2;
+    System.out.println(jumlah);
   }
 }
 
